4d1e6fd3019fe833fcbdb1a214f320ceb95bd2bb
VicenteQueiroz <vicentetqueiroz@gmail.com>
2024-03-29 21:43:43 +0000

    Added modal

--- src/components/databases.py
+++ src/components/databases.py
@@ -1,5 +1,42 @@
-from textual.app import ComposeResult
-from textual.widgets import Static, Button
+from textual.app import App, ComposeResult
+from textual.widgets import Static, Button, Label, Input
+from textual.screen import ModalScreen
+from textual.containers import Grid
+from textual.widget import Widget
+
+class LabelledInput(Widget):
+    DEFAULT_CSS = """
+    LabelledInput {
+        height: 4;
+    }
+    """
+
+    def __init__(self, label):
+        super().__init__()
+        self.label = label
+
+    def compose(self):
+        yield Label(f"{self.label}:")
+        yield Input(placeholder=self.label.lower())
+
+class AddDatabaseScreen(ModalScreen):
+    """Screen with a dialog to add a Redis Database."""
+
+    def compose(self) -> ComposeResult:
+        yield LabelledInput("Host")
+        yield LabelledInput("Port")
+        yield LabelledInput("Database Alias")
+        yield LabelledInput("Username")
+        yield LabelledInput("Password")
+        # yield Button("Test Connection") // TODO
+        yield Button("Cancel", variant="error", id="quit")
+        yield Button("Apply Changes", variant="primary", id="cancel")
+
+    def on_button_pressed(self, event: Button.Pressed) -> None:
+        if event.button.id == "quit":
+            self.app.exit()
+        else:
+            self.app.pop_screen()
 
 
 class Databases(Static):
@@ -7,7 +44,8 @@ class Databases(Static):
 
     # def on_mount(self) -> None:
         # self.update(f"Hello, [b]World[/b]!")
+    
 
     def compose(self) -> ComposeResult:
         """Create child widgets of a stopwatch."""
-        yield Button("Start", id="start", variant="success")
+        yield Button("+ Add Database", id="add_database")
--- src/redis-terminal-viewer.py
+++ src/redis-terminal-viewer.py
@@ -1,18 +1,29 @@
 from textual.app import App, ComposeResult
-from textual.widgets import Footer
+from textual.widgets import Header, Footer, Button, Footer, Header
 
-from components.databases import Databases
+from components.databases import Databases, AddDatabaseScreen
+
+from textual.app import App, ComposeResult
 
 
 class RedisTerminalViewer(App):
     """A Textual app to view Redis Database."""
 
+    CSS_PATH = "styles.tcss"
     BINDINGS = [("d", "toggle_dark", "Toggle dark mode")]
+    TITLE = "Redis Terminal Viewer"
 
     def compose(self) -> ComposeResult:
         """Create child widgets for the app."""
-        yield Footer()
+        yield Header()
         yield Databases()
+        yield Footer()
+        # yield Button("+ Add Database", id="add_database")
+
+    def on_button_pressed(self, event: Button.Pressed) -> None:
+        """Event handler called when a button is pressed."""
+        print("Add database!")
+        self.push_screen(AddDatabaseScreen())
 
     def action_toggle_dark(self) -> None:
         """An action to toggle dark mode."""
